gentoo-qt/qting-edge

commit 62d25aa94f9bd878717032f918a3c6d4766b520e

qt4-build-edge.eclass: respect CC, CXX, LINK (bug #275710).

eclass/qt4-build-edge.eclass
@@ -331,13 +331,18 @@ standard_configure_options() {
 # @DESCRIPTION:
 # Compiles the code in $QT4_TARGET_DIRECTORIES
 build_directories() {
-	local dirs="$@"
-	for x in ${dirs}; do
+	sed -i -e "s:\$\$\[QT_INSTALL_LIBS\]:${QTLIBDIR}:g" \
+		$(find "${S}" -name '*.pr[io]') "${S}"/mkspecs/common/linux.conf \
+		|| die "failed to fix QT_INSTALL_LIBS"
+
+	for x in "$@"; do
 		cd "${S}"/${x}
-		sed -i -e "s:\$\$\[QT_INSTALL_LIBS\]:${QTLIBDIR}:g" \
-			$(find "${S}" -name '*.pr[io]') "${S}"/mkspecs/common/linux.conf \
-			|| die "failed to fix QT_INSTALL_LIBS"
-		"${S}"/bin/qmake "LIBS+=-L${QTLIBDIR}" "CONFIG+=nostrip" || die "qmake failed"
+		"${S}"/bin/qmake "LIBS+=-L${QTLIBDIR}" \
+			QMAKE_CC=$(tc-getCC) \
+			QMAKE_CXX=$(tc-getCXX) \
+			QMAKE_LINK=$(tc-getCXX) \
+			QMAKE_STRIP= \
+			|| die "qmake failed"
 		emake || die "emake failed"
 	done
 }

gentoo/qt

commit 0e30a19cc678141695d4e50c0ea49aea66646633

qt4-build-edge.eclass: revert back to using "CONFIG+=nostrip".
Fixes a regression caused by commit 62d25aa94f9bd878717032f918a3c6d4766b520e,
QMAKE_STRIP is apparently ignored when building Qt libraries...

eclass/qt4-build-edge.eclass
@@ -337,11 +337,12 @@ build_directories() {
 
 	for x in "$@"; do
 		cd "${S}"/${x}
-		"${S}"/bin/qmake "LIBS+=-L${QTLIBDIR}" \
+		"${S}"/bin/qmake \
+			"CONFIG+=nostrip" \
+			"LIBS+=-L${QTLIBDIR}" \
 			QMAKE_CC=$(tc-getCC) \
 			QMAKE_CXX=$(tc-getCXX) \
 			QMAKE_LINK=$(tc-getCXX) \
-			QMAKE_STRIP= \
 			|| die "qmake failed"
 		emake || die "emake failed"
 	done