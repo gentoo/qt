gentoo/qt

commit 52bcb4af3258474d1be5bd6c24c1126d2b915b86

[dev-qt/qtcore:4] Remove redundant code.
We always run qmake with CONFIG+=nostrip, so these lines are unnecessary.

Package-Manager: portage-2.2.10

dev-qt/qtcore/qtcore-4.8.9999.ebuild
@@ -19,7 +19,7 @@ IUSE="+glib iconv icu qt3support ssl"
 DEPEND="
 	sys-libs/zlib
 	glib? ( dev-libs/glib:2 )
-	icu? ( >=dev-libs/icu-49:= )
+	icu? ( dev-libs/icu:= )
 	ssl? ( dev-libs/openssl:0 )
 "
 RDEPEND="${DEPEND}"
@@ -66,11 +66,6 @@ QT4_EXTRACT_DIRECTORIES="${QT4_TARGET_DIRECTORIES}
 QCONFIG_DEFINE="QT_ZLIB"
 
 src_prepare() {
-	# Don't pre-strip, bug 235026
-	for i in kr jp cn tw; do
-		echo "CONFIG+=nostrip" >> "${S}"/src/plugins/codecs/${i}/${i}.pro
-	done
-
 	qt4-build-multilib_src_prepare
 
 	# bug 172219

commit 21a8165cd4a195750ba0c85252ab5506cce5ed6a

[dev-qt/qtcore:4] Explicitly list all codecs in QT4_TARGET_DIRECTORIES.
As a side effect, qmake is run inside each of the codecs dirs,
fixing a "pre-stripped files" regression introduced in commit
52bcb4af3258474d1be5bd6c24c1126d2b915b86.

Package-Manager: portage-2.2.10

dev-qt/qtcore/qtcore-4.8.6.ebuild
@@ -40,7 +40,10 @@ QT4_TARGET_DIRECTORIES="
 	src/corelib
 	src/network
 	src/xml
-	src/plugins/codecs
+	src/plugins/codecs/cn
+	src/plugins/codecs/jp
+	src/plugins/codecs/kr
+	src/plugins/codecs/tw
 	tools/linguist/lconvert
 	tools/linguist/lrelease
 	tools/linguist/lupdate"

dev-qt/qtcore/qtcore-4.8.9999.ebuild
@@ -40,7 +40,10 @@ QT4_TARGET_DIRECTORIES="
 	src/corelib
 	src/network
 	src/xml
-	src/plugins/codecs
+	src/plugins/codecs/cn
+	src/plugins/codecs/jp
+	src/plugins/codecs/kr
+	src/plugins/codecs/tw
 	tools/linguist/lconvert
 	tools/linguist/lrelease
 	tools/linguist/lupdate"